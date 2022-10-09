ad2ebf8e8ae7578023baede8026c83a3d999d6f6
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-10-09 12:16:54 +0600

    team

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -10,7 +10,7 @@ const Nav = () => {
    <div className="sticky top-0 z-50 ">
 
 <div className="mx-auto bg-base-100">
-     <div className="grid sm:grid-cols-1 gap-2 lg:grid-cols-3 items-center justify-items-center  " >
+     <div className="grid  grid-cols-3 gap-2 lg:grid-cols-3 items-center justify-items-center  " >
 
 
 <div>
@@ -30,13 +30,13 @@ const Nav = () => {
   <div>
   <div className="flex items-center font-bold    gap-2" style={{color: "#453364" }}>
         <div>
-          < FaPhoneVolume  className="text-2xl animate-ping	  "/>
+          < FaPhoneVolume  className="text-[10px] lg:text-2xl animate-ping	  "/>
         </div>
         <div>
-          <p className="text-sm   	"  >
+          <p className="text-[10px] lg:text-sm   	"  >
             01619848555
           </p>
-          <p className="text-sm  	 	"  >
+          <p className="text-[10px] lg:text-sm	 	"  >
             info@hcah.mrg.com.bd
           </p>
         </div>
@@ -46,11 +46,11 @@ const Nav = () => {
       <div>
       <div className="flex items-center gap-2  " style={{color: "#453364" }}>
         <div className="animate-spin">
-          <FcAlarmClock className="text-2xl   "></FcAlarmClock>
+          <FcAlarmClock className="text-[15px] lg:text-2xl   "></FcAlarmClock>
         </div>
         <div>
           <div className="	">
-            <p className="text-sm   font-bold  	"  >
+            <p className="text-[10px] lg:text-sm font-bold  	"  >
               24/7days Open
             </p>
       
@@ -61,10 +61,10 @@ const Nav = () => {
       <div className="flex gap-2 items-center" style={{color: "#453364" }}>
       <div>
  
- <BiLocationPlus className="text-2xl   animate-spin"/>
+ <BiLocationPlus className="text-[15px] lg:text-2xl  animate-spin"/>
 </div>
 <div>
- <p className="text-sm   font-bold 		"  >
+ <p className="text-[10px] lg:text-sm font-bold 		"  >
    House 11, Road 108, Gulshan 2,<br></br> Dhaka, Bangladesh. 1212
  </p>
 </div>
