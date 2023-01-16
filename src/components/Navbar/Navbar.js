4eea34bf1f7705e9244d2b1d70e37499bf314977
Sadnan-2019 <sadnan.wow@gmail.com>
2023-01-16 17:19:31 +0600

    services responcive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   const navItem = (
     <>
-    <li className="nav-item p-2 	">
+      <li className="nav-item p-2 	">
         <NavLink
           className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
           to="/home"
@@ -60,7 +60,7 @@ const Navbar = () => {
           Caregiver
         </NavLink>
       </li>
-      
+
       <li className="nav-item p-2">
         <NavLink
           className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
@@ -74,7 +74,7 @@ const Navbar = () => {
           className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
           to="/companion"
         >
-          Companionship 
+          Companionship
         </NavLink>
       </li>
       <li className="nav-item p-2">
@@ -86,124 +86,132 @@ const Navbar = () => {
         </NavLink>
       </li>
 
-    
-      <li className="dropdown dropdown-hover hover:border-none" style={{ backgroundColor: "#453364" }} > 
-       
-  <div className="flex items-center">
-  <label tabIndex={0} className="m-1 text-white" style={{ backgroundColor: "#453364" }}>More</label>
-  <svg
+      <li
+        className="dropdown dropdown-hover hover:border-none"
+        style={{ backgroundColor: "#453364" }}
+      >
+        <div className="flex items-center">
+          <label
+            tabIndex={0}
+            className="m-1 text-white"
+            style={{ backgroundColor: "#453364" }}
+          >
+            More
+          </label>
+          <svg
             class="fill-current h-4 w-4 text-white"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
           >
             <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />{" "}
           </svg>
-  </div>
-  
-  
-  
-  <ul tabIndex={0} className="w-24 dropdown-content menu p-2 shadow  rounded-box lg:w-52" style={{ backgroundColor: "#453364" }}>
-  <li className="nav-item p-2">
-        <NavLink
-          className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
-          to="/blog"
-        >
-          Blog
-        </NavLink>
-      </li>
-      <li className="nav-item p-2">
-        <NavLink
-          className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
-          to="/about"
-        >
-          About
-        </NavLink>
-      </li>
-      <li className="nav-item p-2">
-        <NavLink
-          className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
-          to="/contact"
-        >
-          Contact
-        </NavLink>
-      </li>
-  </ul>
- 
+        </div>
 
-
+        <ul
+          tabIndex={0}
+          className="w-24 dropdown-content menu p-2 shadow  rounded-box lg:w-52"
+          style={{ backgroundColor: "#453364" }}
+        >
+          <li className="nav-item p-2">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? `${active}` : `${normal}`
+              }
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+          </li>
+          <li className="nav-item p-2">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? `${active}` : `${normal}`
+              }
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item p-2">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? `${active}` : `${normal}`
+              }
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
       </li>
-
-     
     </>
   );
 
   return (
     <div className="sticky top-0 z-50 header ">
-<Nav/>
-<div className="px-4 md:pl-10  lg:px-14 header flex justify-center items-center " style={{ backgroundColor: "#453364" }}>
-      {/* Navbar start source code from Daisy UI */}
-     
-      <div className="navbar  header ">
-        {/* Navbar left portion */}
-        <div className="">
-          <div className="dropdown">
-            <label tabindex="0" className="btn btn-ghost lg:hidden text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-              
-            </label>
-            <ul
-              tabindex="0"
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow-xl  rounded-box w-52"
-              style={{backgroundColor: "#453364",color:"white" }}
-            >
-              {navItem}
-            </ul>
-          </div>
-          
-        </div>
+      <Nav />
+      <div
+        className="px-4 md:pl-10  lg:px-14 header  flex justify-center items-center gap-56 lg:gap-28 "
+        style={{ backgroundColor: "#453364" }}
+      >
+        {/* Navbar start source code from Daisy UI */}
 
-        <div className=" flex 	  ">
-          <div className="navbar-start hidden   lg:flex      ">
-            <div className="     ">
-              <ul className="menu-horizontal p-0 flex items-center    ">
+        <div className="navbar  header ">
+          {/* Navbar left portion */}
+          <div className="">
+            <div className="dropdown">
+              <label
+                tabindex="0"
+                className="btn btn-ghost lg:hidden text-white"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabindex="0"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow-xl  rounded-box w-52"
+                style={{ backgroundColor: "#453364", color: "white" }}
+              >
                 {navItem}
               </ul>
             </div>
           </div>
-          
+
+          <div className=" flex 	  ">
+            <div className="navbar-start hidden   lg:flex      ">
+              <div className="     ">
+                <ul className="menu-horizontal p-0 flex items-center    ">
+                  {navItem}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        
+
+        <div className=" navbar-end    ">
+          <a
+            className="btn btn-sm bg-green-500 animate-pulse    rounded-full   "
+            href="https://wa.me/+8801741337397"
+            // href="https://wa.me/+8801767564737"
+            title="WHATSAPP CHAT"
+          >
+            {" "}
+            <BsWhatsapp></BsWhatsapp>
+          </a>
+        </div>
       </div>
-
-      <div className="  ">
-          
-            <a
-              className="btn btn-sm bg-green-500 animate-pulse    rounded-full   "
-              href="https://wa.me/+8801741337397"
-                // href="https://wa.me/+8801767564737"
-              title="WHATSAPP CHAT"
-            >
-              {" "}
-              <BsWhatsapp></BsWhatsapp>
-            </a>
-
-           
-           
-        </div>
-    </div>
     </div>
   );
 };
