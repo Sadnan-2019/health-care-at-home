1dbc8ef53f99019e9dd753642e6c6c9ad05ca3f8
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-15 16:59:32 +0600

    services responcive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const active =
-    "text-white mx-1 font-medium border-b-2 border-white-900 pb-1 md:text-white-900 ";
+    "text-white  font-medium border-b-2 border-white-900 pb-1 md:text-white-900 ";
   const normal =
     "   text-white-600 font-medium font-bold focus:text-white p-0 lg:text-white   ";
 
@@ -89,7 +89,7 @@ const Navbar = () => {
       <li className="nav-item p-2">
         <NavLink
           className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
-          to="/medical-equipment"
+          to="/payment"
         >
           Payment
         </NavLink>
