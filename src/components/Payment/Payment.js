05f73c0f0cca77d73096276d8acce926ab2ed9bd
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-16 14:32:23 +0600

    services responcive

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -6,6 +6,7 @@ import { AiFillTwitterCircle } from "react-icons/ai";
 import { FaWhatsappSquare } from "react-icons/fa";
 import bkash from "../../assets/bkash.png"
 import EFT from "../../assets/EFT.webp"
+import CASH from "../../assets/CASH.jpg"
 import 'animate.css';
  
 
@@ -20,29 +21,42 @@ const Payment = () => {
       </h2>
 
       <div className=" grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3  gap-4 px-12">
-        <div class="card  bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white mb-5 mt-12">
+        <div class="card animate__animated animate__backInRight   bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white mb-5 mt-12">
           <div class="card-body">
-             <img src={bkash} className="animate__animated animate__backInRight      rounded-md 	" alt='' />
+             <img src={bkash} className="w-92 h-32      rounded-md 	" alt='' />
             {/* <div className="flex gap-3    mt-5">
            < BsFacebook  className="lg:text-2xl text-[20px] " style={{  color:"white" }}/>
             < RxLinkedinLogo  className="lg:text-2xl text-[20px] " style={{  color:"white" }}/>
             < AiFillTwitterCircle  className="lg:text-2xl text-[20px] " style={{  color:"white" }}/>
             < FaWhatsappSquare  className="lg:text-2xl text-[20px] " style={{  color:"white" }}/>
            </div>  */}
+           <h2
+        className="text-3xl text-center py-6  font-bold poppins-b sm:text-4xl"
+        style={{ color: "white" }}
+      >
+Bkash Merchant Payment      </h2>
           </div>
           
         </div>
-        <div class="card  bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white mb-5 mt-12">
+        <div class="card animate__animated   animate__bounceInUp  bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white mb-5 mt-12">
           <div class="card-body">
-             <img src={EFT} className=" animate__animated animate__backInRight  rounded-md	" alt=''/>
-             
+             <img src={EFT} className="w-92 h-32  rounded-md	" alt=''/>
+             <h2
+        className="text-3xl text-center py-6  font-bold poppins-b sm:text-4xl"
+        style={{ color: "white" }}
+      >
+Electronic Money Transfer     </h2>
           </div>
           
         </div>
-        <div class="card  bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white mb-5 mt-12">
+        <div class="card animate__animated animate__backInLeft bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white mb-5 mt-12">
           <div class="card-body">
-             <img src={EFT} className=" animate__animated animate__backInRight  rounded-md	" alt=''/>
-             
+             <img src={CASH} className="w-92 h-32   rounded-md	" alt=''/>
+             <h2
+        className="text-3xl text-center py-6  font-bold poppins-b sm:text-4xl"
+        style={{ color: "white" }}
+      >
+Cash Payment   </h2>
           </div>
           
         </div>
