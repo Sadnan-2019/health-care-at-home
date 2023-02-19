3b3d06e77045abfe1b9271bfd7fc66d6b11e82c6
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-19 13:58:29 +0600

    services responcive

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import Payment from "./components/Payment/Payment";
 import CardiacSymptomsCauses from "./components/Blog/CardiacSymptomsCauses";
 import DiagnosisTests from "./components/Blog/DiagnosisTests";
 import ManagementTreatment from "./components/Blog/ManagementTreatment";
+import Prevention from "./components/Blog/Prevention";
 
 
  
@@ -85,6 +86,7 @@ function App() {
         <Route path="/cardiac-symptoms-causes" element={<CardiacSymptomsCauses />}></Route>
         <Route path="/diagnosis-tests" element={<DiagnosisTests />}></Route>
         <Route path="/management-treatment" element={<ManagementTreatment />}></Route>
+        <Route path="/prevention" element={<Prevention />}></Route>
  
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
--- src/components/Blog/PhysioBlog.js
+++ src/components/Blog/PhysioBlog.js
@@ -227,7 +227,7 @@ const PhysioBlog = () => {
                 </div>
                 <div className="flex gap-5">
                 <div className="py-2       ">
-                  <Link to="/blogone">
+                  <Link to="/prevention">
                     <button
                       htmlFor="my-modal-6"
                       className="btn btn-sm modal-button mt-3 text-[white] bg-gradient-to-r from-[#303640] to-[#103264]   animate-pulse    "
