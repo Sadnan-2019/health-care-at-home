a2fb3cb24d1b40bee688e2c213d3dc094a865bfa
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-19 16:26:38 +0600

    services responcive

--- src/components/Blog/PhysioBlog.js
+++ src/components/Blog/PhysioBlog.js
@@ -17,138 +17,12 @@ const PhysioBlog = () => {
       <div>
         <div
           className="py-10"
-          // style={{ backgroundColor: "#453364" }}
+          
         >
-          {/* <div className=" ">
-            <h2 className="text-3xl text-center text-white  font-bold poppins-b sm:text-4xl">
-              Meet Our Experts Physiotherapist
-            </h2>
-            <p className=" mt-7  md:text-[15px] lg:text-[27px] poppins-t text-white text-justify px-4">
-              Our highly qualified experts devise personalized physiotherapy
-              plans for smooth, early, and effective relief from pain.
-            </p>
-          </div> */}
+           
           <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2  mx-10 gap-10  items-center justify-items-center  py-12">
-            {/* <div
-              className="card  border  bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white   "
-              data-aos="flip-left"
-              data-aos-easing="ease-out-cubic"
-              data-aos-duration="2000"
-              style={{ height: "600px" }}
-            >
-              <div className="       ">
-                <figure className="">
-                  <img
-                    src={cardiac1}
-                    alt="Shoes"
-                    className="  p-4  "
-                    style={{ height: "250px", borderRadius: "25px" }}
-                  />
-                </figure>
-                <div className="  text-white p-2 ">
-                  <h2 className="text-2xl text-center  font-bold">
-                    Can You Prevent Sudden Cardiac Arrest?
-                  </h2>
-                </div>
-
-                <div className="mx-10 flex items-center justify-items-center gap-10 animate__animated animate__backInLeft">
-                  <div>
-                    <div className="flex items-center justify-start  ">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                       </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Weight Losses.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Straise Management.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Control HTM, DM.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Foods Habit.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        ECG & Angiogram Check Every Year.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Cut Red meat.{" "}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div>
-                    <div className="flex items-center justify-start  ">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Avoids Smoking, Alchohol.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Check Cholostorel.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Take Baby aspirin every day.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Regular exercise.
-                      </p>
-                    </div>
-                    <div className="flex items-center justify-start">
-                      <span>
-                        <AiOutlineCaretRight className="text-2xl text-white-900"></AiOutlineCaretRight>
-                      </span>
-                      <p className="text-[15px] font-medium text-justify py-2 ml-3">
-                        Cut Down Saturated fat.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-            <div class="card  bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white mb-5 mt-12">
+           
+            <div class="card  bg-gradient-to-r from-[#486038] to-[#38235D]   text-white mb-5 mt-12">
               <img
                 src={cardiac1}
                 alt=""
@@ -189,7 +63,7 @@ const PhysioBlog = () => {
                   <Link to="/cardiac-symptoms-causes">
                     <button
                       htmlFor="my-modal-6"
-                      className="btn btn-sm modal-button mt-3 text-[white] bg-gradient-to-r from-[#303640] to-[#103264]   animate-pulse    "
+                      className="btn lg:btn-sm modal-button mt-3 text-[white] bg-gradient-to-r from-[#303640] to-[#103264]   animate-pulse    "
                       style={{ backgroundColor: "#453364" }}
                     >
                       Symptoms and Causes{" "}
@@ -200,7 +74,7 @@ const PhysioBlog = () => {
                   <Link to="/diagnosis-tests">
                     <button
                       htmlFor="my-modal-6"
-                      className="btn btn-sm modal-button mt-3 text-[white] bg-gradient-to-r from-[#303640] to-[#103264]   animate-pulse    "
+                      className="btn lg:btn-sm modal-button mt-3 text-[white] bg-gradient-to-r from-[#303640] to-[#103264]   animate-pulse    "
                       style={{ backgroundColor: "#453364" }}
                     >
                       Diagnosis and Tests{" "}
@@ -212,7 +86,7 @@ const PhysioBlog = () => {
                     {/* Management and Treatment */}
                     <button
                       htmlFor="my-modal-6"
-                      className="btn btn-sm modal-button mt-3 text-[white] bg-gradient-to-r from-[#303640] to-[#103264]   animate-pulse    "
+                      className="btn lg:btn-sm modal-button mt-3 text-[white] bg-gradient-to-r from-[#303640] to-[#103264]   animate-pulse    "
                       style={{ backgroundColor: "#453364" }}
                     >
                       Management and Treatment{" "}
