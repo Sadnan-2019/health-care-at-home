8d26d0aebde3ecac40e0ab2238aeb511bd3af981
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-20 13:57:58 +0600

    services responcive

--- src/components/Blog/ManagementTreatment.js
+++ src/components/Blog/ManagementTreatment.js
@@ -17,13 +17,15 @@ const ManagementTreatment = () => {
             <h2 className="text-3xl font-bold ">
               How is sudden cardiac arrest treated?
             </h2>
-            <p className="text-1xl font-bold">
+            <p className="text-1xl font-bold bg-gradient-to-r from-[#486038] to-[#38235D]   text-white
+            p-6 rounded-lg shadow-lg   m-4
+            ">
               You can treat and reverse sudden cardiac arrest. However,
               emergency action has to start immediately. Survival can be as high
               as 90% if treatment starts within the first minutes after sudden
               cardiac arrest. The rate drops by about 10% each minute longer.
             </p>
-            <p className="text-1xl font-bold mt-2">
+            <p className="p-6 rounded-lg shadow-lg   m-4 text-1xl font-bold mt-2 bg-gradient-to-r from-[#486038] to-[#38235D]   text-white">
               If you see someone experiencing sudden cardiac arrest, do this:
               Start CPR, even if it’s just the hands-only version. CPR can save
               a life. It keeps blood and oxygen circulating until help arrives.
@@ -33,7 +35,7 @@ const ManagementTreatment = () => {
               person survive. The shorter the time until defibrillation, the
               greater the chance of survival.
             </p>
-            <p className="mt-2 font-bold">
+            <p className="p-6 rounded-lg shadow-lg   m-4 mt-2 font-bold bg-gradient-to-r from-[#486038] to-[#38235D]   text-white">
               Once emergency personnel arrive, defibrillation can restart the
               person’s heart if they haven’t received a shock from an AED yet.
               Defibrillators shock your heart through paddles placed on your
