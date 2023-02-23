6f38ef28dafeae4f69f26497f5e3bccddc5c895c
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-23 10:45:07 +0600

    services responcive

--- src/components/Blog/Prevention.js
+++ src/components/Blog/Prevention.js
@@ -1,31 +1,39 @@
 import React from "react";
 import "animate.css";
 import heart1 from "../../assets/Risk.avif";
-// import heart2 from "../../assets/sudden-cardiac-arrest.jpg";
-// import heart3 from "../../assets/sudden-cardiac-arrest2.png";
+import "./Blog.css";
+import(
+  "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;1,300&display=swap"
+);
 const Prevention = () => {
   return (
     <div>
-      <div className=" py-5">
+      <div className=" py-5 blog">
         <p className="text-3xl font-bold    px-10">PREVENTION </p>
         <p className="text-xl      px-10">Healthcare at Home</p>
         <p className="text-xl      px-10">09 January 2023, 21:55</p>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10  mx-10  items-center	 px-10 ">
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-10  mx-10  items-center	 px-10 ">
           <div className=" ">
             <img className="py-5  px-10 rounded-md  	" src={heart1} alt="" />
           </div>
-          <div className="py-10 bg-gradient-to-r from-[#486038] to-[#38235D]   text-white p-6 rounded-lg shadow-lg   m-4">
+          
+        </div>
+
+        <div  className="grid grid-cols-1 lg:grid-cols-1 gap-10  mx-10  items-center	 px-10 ">
+
+        <div className="py-10 bg-gradient-to-r from-[#486038] to-[#38235D]   text-white p-6 rounded-lg shadow-lg   m-4">
             <h2 className="text-3xl font-bold ">How can I reduce my risk?</h2>
             {/* <p className="text-1xl font-bold">
               In more than half of the cases, sudden cardiac arrest happens
               without prior symptoms.
             </p> */}
-            <p className="text-1xl font-bold mt-3">
+            <p className="text-xl mt-3">
               You can reduce your risk in many ways, such as:
             </p>
 
-            <div className="text-1xl font-bold  ">
-              <li>
+            <div className="text-xl  ">
+             <div>
+             <li>
                 Keeping your regular follow-up appointments with your healthcare
                 provider.
               </li>
@@ -51,8 +59,10 @@ const Prevention = () => {
                 Teaching your family the importance of immediate care and
                 learning CPR.
               </li>
+             </div>
 
-              <p className="text-1xl font-bold mt-2 ">
+              <div>
+              <p className="text-xl mt-2 ">
                 High school and college athletes should have a heart screening
                 every two years. This should include an evaluation of their
                 personal and family history (updated annually) and a physical
@@ -60,6 +70,7 @@ const Prevention = () => {
                 such as an electrocardiogram if there are any concerning
                 findings on an initial evaluation.
               </p>
+              </div>
             </div>
           </div>
         </div>
