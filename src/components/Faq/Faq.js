e9918e4cee6075f59a3d9ca4570db39bab8abed8
Sadnan-2019 <sadnan.wow@gmail.com>
2024-03-27 15:51:01 +0600

    uk

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -3,103 +3,103 @@ import React from "react";
 const Faq = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center justify-items-center py-8">
-        <div className="mx-[80px]">
-          <h2
-            className="text-3xl text-center   font-bold poppins-b sm:text-4xl py-5"
-            style={{ color: "#453364" }}
-          >
-            Frequently Asked Questions
-          </h2>
-          <div
-            tabIndex={0}
-            className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
-          >
-            <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
-              I would like to get all services at home. How is it possible?
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center justify-items-center py-8">
+          <div className="mx-[80px]">
+            <h2
+              className="text-3xl text-center   font-bold poppins-b sm:text-4xl py-5"
+              style={{ color: "#453364" }}
+            >
+              Frequently Asked Questions
+            </h2>
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                I would like to get all services at home. How is it possible?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <li>Contact us and talk to our expert</li>
+                <li>Select service after baseline evaluation</li>
+                <li>Paying for the service</li>
+                <p className="mt-5">
+                  Our expert will suggest what is best for your specific needs:
+                  Recovery Program at home or Center based care.
+                </p>
+              </div>
             </div>
-            <div className="collapse-content text-[15px] lg:text-[18px]">
-              <li>Contact us and talk to our expert</li>
-              <li>Select service after baseline evaluation</li>
-              <li>Paying for the service</li>
-              <p className="mt-5">
-                Our expert will suggest what is best for your specific needs:
-                Recovery Program at home or Center based care.
-              </p>
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                I am not sure which program fits my needs. What should I do?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <p className="mt-5">
+                  Talk to our expert on 01619848555 or visit your nearest HCAH
+                  Care Center
+                </p>
+                <p className="mt-5">
+                  They will understand your requirement and suggest the best
+                  course of action
+                </p>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                Are all your professionals medically trained?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <p className="mt-5 text-justify">
+                  We have certified,registered, and experienced professionals
+                  including doctors,nurses,physiotherapists,caregiver,medical
+                  assistant,medical caregiver.
+                </p>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                What is the difference between recovery at the HCAH care center
+                and at the hospital?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <p className="mt-5 text-justify">
+                  The objective of treatment at the hospital is to save your life.
+                  You are discharged from the hospital once you are medically
+                  stable..
+                </p>
+                <p className="mt-5 text-justify">
+                  The objective of the HCAH care center is to rebuild the
+                  physical, functional, and cognitive abilities you've lost
+                  because of the disease and its the treatment so that you can
+                  return to normal life again.
+                </p>
+                <p className="mt-5 text-justify">
+                  When you are discharged from the HCAH care center, you are not
+                  just medically stable, you are physically, mentally, and
+                  emotionally healthy.
+                </p>
+              </div>
             </div>
           </div>
-          <div
-            tabIndex={0}
-            className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
-          >
-            <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
-              I am not sure which program fits my needs. What should I do?
-            </div>
-            <div className="collapse-content text-[15px] lg:text-[18px]">
-              <p className="mt-5">
-                Talk to our expert on 01619848555 or visit your nearest HCAH
-                Care Center
-              </p>
-              <p className="mt-5">
-                They will understand your requirement and suggest the best
-                course of action
-              </p>
-            </div>
-          </div>
-          <div
-            tabIndex={0}
-            className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
-          >
-            <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
-              Are all your professionals medically trained?
-            </div>
-            <div className="collapse-content text-[15px] lg:text-[18px]">
-              <p className="mt-5 text-justify">
-                We have certified,registered, and experienced professionals
-                including doctors,nurses,physiotherapists,caregiver,medical
-                assistant,medical caregiver.
-              </p>
-            </div>
-          </div>
-          <div
-            tabIndex={0}
-            className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
-          >
-            <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
-              What is the difference between recovery at the HCAH care center
-              and at the hospital?
-            </div>
-            <div className="collapse-content text-[15px] lg:text-[18px]">
-              <p className="mt-5 text-justify">
-                The objective of treatment at the hospital is to save your life.
-                You are discharged from the hospital once you are medically
-                stable..
-              </p>
-              <p className="mt-5 text-justify">
-                The objective of the HCAH care center is to rebuild the
-                physical, functional, and cognitive abilities you've lost
-                because of the disease and its the treatment so that you can
-                return to normal life again.
-              </p>
-              <p className="mt-5 text-justify">
-                When you are discharged from the HCAH care center, you are not
-                just medically stable, you are physically, mentally, and
-                emotionally healthy.
-              </p>
-            </div>
+          <div>
+            <img
+              className="rounded"
+              src="https://www.tbsnews.net/sites/default/files/styles/big_2/public/images/2020/04/07/burn_hospital_salahuddin_ahmed_2.jpg?itok=-XDbK1lX&timestamp=1586250124"
+              alt=""
+              data-aos="flip-left"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="2000"
+            />
           </div>
         </div>
-        <div>
-          <img
-            className="rounded"
-            src="https://www.tbsnews.net/sites/default/files/styles/big_2/public/images/2020/04/07/burn_hospital_salahuddin_ahmed_2.jpg?itok=-XDbK1lX&timestamp=1586250124"
-            alt=""
-            data-aos="flip-left"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-          />
-        </div>
-      </div>
     </div>
   );
 };
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,143 +5,103 @@ import schs from "../../assets/schs.png";
 import { FcAlarmClock } from "react-icons/fc";
 import { FaPhoneVolume } from "react-icons/fa";
 import { BiLocationPlus } from "react-icons/bi";
-import HCAHBD from "../../assets/HCAHBD.png"
+import HCAHBD from "../../assets/HCAHBD.png";
 import { useState } from "react";
 import "animate.css";
-import 'animate.css';
-
-
-
+import "animate.css";
 
 const Nav = () => {
-
   // const [addclose, setClose] = useState(true);
 
-  
   return (
-   <div className="sticky top-0 z-50 blog ">
-
-<div className="mx-auto bg-base-100">
-     <div className="   grid    grid-cols-2 lg:gap-10 lg:grid-cols-4 items-center justify-items-center  " >
-
-
-<div>
-
-
-<Link to="/">
-            <div className="  flex items-center">
-              <img
-                className="w-48"
-                alt="logo"
-                src={HCAHBD}
-              />
-            </div>
-          </Link>
-</div>
-
-  <div>
-  <div className="flex items-center font-bold    gap-2" style={{color: "#453364" }}>
-        <div>
-          < FaPhoneVolume  className="text-[10px] lg:text-2xl animate-ping	  "/>
-        </div>
-        <div>
-          <p className="text-[10px]   font-bold lg:text-xs    bg-clip-text   	"  >
-             
-            <a href="tel:+01619-848555 " className="  	">
-            01619848555
-                  </a>/
-            <a href="tel:+01779848555 " className="  	">
-            01779848555
-                  </a>
-          </p>
-          <p className="text-[10px]   font-bold lg:text-xs    bg-clip-text	 	"  >
-            info@hcah.mrg.com.bd
-          </p>
-        </div>
-      </div>
-      
-  </div>
-      <div className="  mx-3">
-      <div className="flex items-center gap-2  " style={{color: "#453364" }}>
-        <div className="animate-spin">
-          <FcAlarmClock className="text-[15px] lg:text-2xl   "></FcAlarmClock>
-        </div>
-        <div>
-          <div className="	">
-            <p className="text-[10px]   font-bold lg:text-xs    bg-clip-text 	"  >
-              24/7days Open
-            </p>
-      
+    <div className="sticky top-0 z-50 blog ">
+      <div className="mx-auto bg-base-100">
+        <div className="   grid    grid-cols-2 lg:gap-10 lg:grid-cols-4 items-center justify-items-center  ">
+          <div>
+            <Link to="/">
+              <div className="  flex items-center">
+                <img className="w-48" alt="logo" src={HCAHBD} />
+              </div>
+            </Link>
           </div>
-        </div>
-        
-      </div>
-      <div className="flex gap-2 items-center" style={{color: "#453364" }}>
-      <div>
- 
- <BiLocationPlus className="text-[15px] lg:text-2xl  animate-spin"/>
-</div>
-<div>
- <p className="text-[10px]   font-bold 	 lg:text-xs    bg-clip-text	"  >
-   House 11, Road 108, Gulshan 2,<br></br> Dhaka, Bangladesh. 1212
- </p>
-</div>
-      </div>
-      </div>
-      <div
+
+          <div>
+            <div
+              className="flex items-center font-bold    gap-2"
+              style={{ color: "#453364" }}
+            >
+              <div>
+                <FaPhoneVolume className="text-[10px] lg:text-2xl animate-ping	  " />
+              </div>
+              <div>
+                <p className="text-[10px]   font-bold lg:text-xs    bg-clip-text   	">
+                  <a href="tel:+01619-848555 " className="  	">
+                    01619848555
+                  </a>
+                  /
+                  <a href="tel:+01779848555 " className="  	">
+                    01779848555
+                  </a>
+                </p>
+                <p className="text-[10px]   font-bold lg:text-xs    bg-clip-text	 	">
+                  info@hcah.mrg.com.bd
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="  mx-3">
+            <div
+              className="flex items-center gap-2  "
+              style={{ color: "#453364" }}
+            >
+              <div className="animate-spin">
+                <FcAlarmClock className="text-[15px] lg:text-2xl   "></FcAlarmClock>
+              </div>
+              <div>
+                <div className="	">
+                  <p className="text-[10px]   font-bold lg:text-xs    bg-clip-text 	">
+                    24/7days Open
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="flex gap-2 items-center"
+              style={{ color: "#453364" }}
+            >
+              <div>
+                <BiLocationPlus className="text-[15px] lg:text-2xl  animate-spin" />
+              </div>
+              <div>
+                <p className="text-[10px]   font-bold 	 lg:text-xs    bg-clip-text	">
+                  House 11, Road 108, Gulshan 2,<br></br> Dhaka, Bangladesh.
+                  1212
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
             className="   lg:flex items-center font-bold  justify-items-center	    gap-2"
             style={{ color: "white" }}
           >
             <div>
-            <p className="text-[5px] sm:m-4 lg:text-xs text-transparent  bg-clip-text bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white 	">
+              <p className="text-[5px] sm:m-4 lg:text-xs text-transparent  bg-clip-text bg-gradient-to-r from-[#486038] to-[#38235D] shadow-xl  text-white 	">
                 Link Organization
               </p>
             </div>
             <div className=" animate__animated animate__backInLeft  ">
-
-              
               {/* <a href="https://www.facebook.com/TMCTrishal" target="_blank" ><img src={sub} className=" ml-6 w-10 lg:w-12 lg:ml-4  	" alt=""/></a> */}
-              <a href="https://schs.edu.bd/" target="_blank" ><img src={schs} className="   w-40 lg:w-28  	" alt=""/></a>
+              <a href="https://schs.edu.bd/" target="_blank">
+                <img src={schs} className="   w-40 lg:w-28  	" alt="" />
+              </a>
               {/* <a href="https://schs.edu.bd/" target="_blank" ><img src={schs} className="   w-40 lg:w-28  	" alt=""/></a> */}
-               
             </div>
           </div>
-     </div>
-      
-      
-    {/* {addclose? (
-      <div className=" grid-cols-1 lg:grid-cols-1  lg:px-72   animate__animated animate__backInRight      ">
- 
-      <div class="     	 py-8  	bg-red-500   text-white rounded-lg shadow-md"  >
-  <div class="flex justify-evenly ">
-   
-    <div className="px-3">
-      <p class="font-bold">Our Payment</p>
-      <p class="text-sm">An error has occurred, please try again later.</p>
-    </div>
-    
-    
-  <div className="px-3">
-  <button type="button" class="text-white font-bold   top-0 bottom-0 right-0  
-  
-  " onClick={() => {
-    setClose(false);
- 
-  }}
-     >
-    Close
-  </button>
-  </div>
-  </div>
-  
-</div>
+        </div>
 
+       
       </div>
-    ):null} */}
-      
-    
     </div>
-   </div>
   );
 };
 
