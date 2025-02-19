d809f0b4a26ecbfe85c8188e0b68eb3343acb369
Sadnan-2019 <sadnan.wow@gmail.com>
2025-02-19 17:58:28 +0600

    seo content

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,7 +3,7 @@ import React from "react";
 const Map = () => {
   return (
     <div className="    ">
-      <iframe
+      <iframe title="map"
         className="broder   lg:w-[500px] md:w-[350px] py-8 rounded  h-96  "
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14603.042712100108!2d90.4186963!3d23.7915351!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa5998bedeec72a77!2sHealthCare%20At%20Home%20Bangladesh!5e0!3m2!1sen!2sbd!4v1666516296223!5m2!1sen!2sbd"
         allowfullscreen=""
